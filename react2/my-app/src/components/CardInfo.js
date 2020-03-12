9dcd41d4454d38a0565c55c5fce92ac320d482b8
Kayleigh Flahertyt <xokaydfxo@gmail.com>
2020-03-11 21:52:51 -0500

    something meaningful to me

--- react2/my-app/src/components/CardInfo.js
+++ react2/my-app/src/components/CardInfo.js
@@ -5,21 +5,26 @@ import Card from './Card.js'
 export default class CardInfo extends Component{
 
 render(){
+let moviesArray = [];
+for (let i = 0; i < this.props.favoriteMovies.length; i++){
+moviesArray.push(<li>{this.props.favoriteMovies[i]}</li>)
+}
+
+
      return (
      <div>
          <h1>something / 25 </h1>
          <div>
-           <h1>Human Name</h1>
-           <h3>From:</h3>
-           <h3>Job Title:</h3>
-           <h3>Employer:</h3>
+           <h1>{this.props.name.first} {this.props.name.last}</h1>
+           <h3>From: {this.props.city},{this.props.country}</h3>
+           <h3>Job Title: {this.props.country}</h3>
+           <h3>Employer: {this.props.employer}</h3>
          </div>
          <div>
-             <h1>Favorite Movies </h1>
+             <h1>Favorite Movies: </h1>
              <ol>
-                 <li></li>
-                 <li></li>
-                 <li></li>
+                 {moviesArray}
+                 
              </ol>
          </div>
          <div>
@@ -28,7 +33,7 @@ render(){
          </div>
      </div>   
      )
-}
+    }
 
 }
 
